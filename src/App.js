bd377c16d6bf9f600658e728e27793be89f0c4d1
SahilThakur7 <sahilrd07@gmail.com>
2017-09-17 11:51:38 +0530

    created components. Will implement fetch tomorrow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'whatwg-fetch';
+import TimeForm from './TimeForm';
 
-const a = [1,10,100,1000];
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            currentTime: null,
+            msg: 'now'
+        }
+    }
 
-const App = (props) => {
-    return (
-        <ul>
-            {React.Children.map(a, i => <li>{i}</li>)}
-        </ul>
-    )
-}
+
+    fetchCurrentTime() {}
+    getApiUrl(){}
+    handleFormSubmit(){}
+    handleChange(newState){}
+
+    render(){
+        const {currentTime,tz} = this.state;
+        const apiUrl = this.getApiUrl();
+
+        return(
+            <div>
+            {!currentTime && <button onClick={this.fetchCurrentTime.bind(this)}>
+                Get The Current Time
+                </button>}
+            {currentTime && <div> The Current Time is: {currentTime}</div>}
+            <TimeForm 
+
+            onFormSubmit={this.handleFormSubmit.bind(this)}
+            onFormChange={this.handleChange.bind(this)}
+            tz={tz}
+            msg={'now'}
+            />
+            <p>We making a request from : <code>{apiUrl}</code></p>
+            </div>
+
+        )
+
+    }}
 
 export default App;
