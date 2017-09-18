a1dd4db9403e3b67c3528ed1fb5d412409d2412b
SahilThakur7 <sahilrd07@gmail.com>
2017-09-18 19:42:14 +0530

    after first routing

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import 'whatwg-fetch';
-import TimeForm from './TimeForm';
-
-class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            currentTime: null,
-            msg: 'now',
-            tz: 'PST'
-        }
-    }
+import {Router, Route, hashHistory} from 'react-router'
 
 
-    fetchCurrentTime() {
-        fetch(this.getApiUrl())
-        .then(resp => resp.json())
-            .then(resp => {
-                const currentTime = resp.dateString;
-                this.setState({currentTime})
-            })
-    }
-    getApiUrl(){
-        const {tz,msg} = this.state;
-        const host = 'https://fullstacktime.herokuapp.com';
-        return `${host}/${tz}/${msg}.json`;
-    }
-    handleFormSubmit(){
-        this.fetchCurrentTime();
-    }
+const Home = () => (<div><h1>WELCOME HOME</h1></div>)
 
-    handleChange(newState){
-        this.setState(newState);
-    }
+export class App extends React.Component {
 
 
     render(){
-        const {currentTime,tz} = this.state;
-        const apiUrl = this.getApiUrl();
 
         return(
-            <div>
-            {!currentTime && <button onClick={this.fetchCurrentTime.bind(this)}>
-                Get The Current Time
-                </button>}
-            {currentTime && <div> The Current Time is: {currentTime}</div>}
-            <TimeForm 
-
-            onFormSubmit={this.handleFormSubmit.bind(this)}
-            onFormChange={this.handleChange.bind(this)}
-            tz={tz}
-            msg={'now'}
-            />
-            <p>We making a request from : <code>{apiUrl}</code></p>
-            </div>
-
+            <Router history={hashHistory} >
+                <Route path="/" component={Home}/>
+            </Router>
         )
 
-    }}
+    } 
+}
 
 export default App;
